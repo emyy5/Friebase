7cff1e0ac4e751f14b3498a058215c0792c78b28
Github <emyy5>
2023-06-17 02:51:36 +0300

    Initial Commit

---
+++ JobApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobApp
+//
+//  Created by Eman Khaled on 17/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
